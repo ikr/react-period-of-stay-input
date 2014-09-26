35c64d2861302652aaf49712b94b183a2d739d58
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-09-26 09:52:23 +0200

    Extract valueInputs() and addModeLinks()

--- src/PeriodOfStayInput.js
+++ src/PeriodOfStayInput.js
@@ -5,40 +5,46 @@
 
     module.exports = React.createClass({
         render: function () {
-            return React.DOM.div({className: 'period-of-stay-input'}, this.topRow());
+            return React.DOM.div(
+                {className: 'period-of-stay-input'}, this.addModeLinks(this.valueInputs()));
         },
 
-        topRow: function () {
+        valueInputs: function () {
+            var m = this.props.model;
+
+            return [
+                React.DOM.label(
+                    {className: 'period-of-stay-check-in', key: 0},
+                    'Check-in day',
+                    React.DOM.input({type: 'date', ref: 'checkIn', value: m.checkInDate})
+                ),
+
+                React.DOM.label(
+                    {className: 'period-of-stay-check-out', key: 1},
+                    'Check-out day',
+                    React.DOM.input({type: 'date', ref: 'checkOut', value: m.checkOutDate})
+                ),
+
+                React.DOM.label(
+                    {className: 'period-of-stay-nights', key: 2},
+                    'Nights',
+
+                    React.DOM.input({
+                        type: 'number',
+                        min: 1,
+                        step: 1,
+                        max: 27,
+                        ref: 'nights',
+                        value: m.nightsCount()
+                    })
+                )
+            ];
+        },
+
+        addModeLinks: function (componentsArray) {
             var m = this.props.model,
                 e = this.props.environment,
-
-                result = [
-                    React.DOM.label(
-                        {className: 'period-of-stay-check-in', key: 0},
-                        'Check-in day',
-                        React.DOM.input({type: 'date', ref: 'checkIn', value: m.checkInDate})
-                    ),
-
-                    React.DOM.label(
-                        {className: 'period-of-stay-check-out', key: 1},
-                        'Check-out day',
-                        React.DOM.input({type: 'date', ref: 'checkOut', value: m.checkOutDate})
-                    ),
-
-                    React.DOM.label(
-                        {className: 'period-of-stay-nights', key: 2},
-                        'Nights',
-
-                        React.DOM.input({
-                            type: 'number',
-                            min: 1,
-                            step: 1,
-                            max: 27,
-                            ref: 'nights',
-                            value: m.nightsCount()
-                        })
-                    )
-                ];
+                result = componentsArray.slice();
 
             if (e.zeroNightsAllowed) {
                 if (m.nightsCount() === 0) {
