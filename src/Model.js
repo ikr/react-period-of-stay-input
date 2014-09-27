7a65004dbab237eb978cc96c610aa242b536b5a2
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-09-27 10:43:23 +0200

    New newCheckIn implementation; simplify

--- src/Model.js
+++ src/Model.js
@@ -10,29 +10,29 @@
 
         maxNightsCount = function () {
             return 27;
-        };
+        },
 
-    if (!Number.isInteger) {
-        Number.isInteger = function isInteger (nVal) {
-            return (
-                typeof nVal === 'number' &&
-                isFinite(nVal) &&
-                nVal > -9007199254740992 &&
-                nVal < 9007199254740992 &&
-                Math.floor(nVal) === nVal
-            );
+        fmt = function () {
+            return 'YYYY-MM-DD';
         };
-    }
 
     Model.prototype.newCheckIn = function (checkInDate, environment) {
-        var mCheckOut = moment(this.checkOutDate, 'YYYY-MM-DD'),
+        var mCheckOut = moment(this.checkOutDate, fmt()),
             mCheckIn;
 
-        if (moment(checkInDate, 'YYYY-MM-DD').isValid()) {
-            mCheckIn = moment(checkInDate, 'YYYY-MM-DD');
+        if (moment(checkInDate, fmt()).isValid()) {
+            mCheckIn = moment(checkInDate, fmt());
 
-            if (mCheckOut.isBefore(mCheckIn)) {
-                return new Model(checkInDate, mCheckIn.add(this.nightsCount(), 'days'));
+            if (
+                mCheckOut.isBefore(mCheckIn) ||
+                (!environment.zeroNightsAllowed && mCheckIn.isSame(mCheckOut, 'day')) ||
+                (mCheckOut.diff(mCheckIn, 'days') > maxNightsCount())
+            ) {
+                return new Model(checkInDate, mCheckIn.add(1, 'days').format(fmt()));
+            }
+
+            if (mCheckIn.isBefore(moment(environment.today, fmt()))) {
+                return this;
             }
 
             return new Model(checkInDate, this.checkOutDate);
@@ -41,51 +41,27 @@
         return this;
     };
 
-    Model.prototype.newCheckOut = function (checkOutDate, environment) {
-        var mCheckIn = moment(this.checkInDate, 'YYYY-MM-DD'),
-            mCheckOut,
-            v;
+    Model.prototype.newCheckOut = function (checkOutDate) {
+        var mCheckIn = moment(this.checkInDate, fmt()),
+            mCheckOut;
 
-        if (moment(checkOutDate, 'YYYY-MM-DD').isValid()) {
-            mCheckOut = moment(checkOutDate, 'YYYY-MM-DD');
-            v = validate(mCheckIn, mCheckOut, environment);
+        if (moment(checkOutDate, fmt()).isValid()) {
+            mCheckOut = moment(checkOutDate, fmt());
 
-            if (v[0]) {
-                return new Model(this.checkInDate, checkOutDate);
+            if (mCheckOut.isBefore(mCheckIn)) {
+                return new Model(mCheckOut.subtract(this.nightsCount(), 'days'), checkOutDate);
             }
-            else {
-                return new Model(
-                    mCheckOut.subtract(1, 'days').format('YYYY-MM-DD'), checkOutDate, v[1]);
-            }
+
+            return new Model(this.checkInDate, checkOutDate);
+
         }
 
-        return new Model(this.checkInDate, this.checkOutDate, 'Invalid check-out day replaced');
-    };
-
-    Model.prototype.newNights = function (count, environment) {
-        var mCheckIn,
-            mCheckOut,
-            v;
-
-        if (Number.isInteger(count) && (Math.abs(count) < 10000)) {
-            mCheckIn = moment(this.checkInDate, 'YYYY-MM-DD');
-            mCheckOut = moment(this.checkInDate, 'YYYY-MM-DD').add(count, 'days');
-            v = validate(mCheckIn, mCheckOut, environment);
-
-            if (v[0]) {
-                return new Model(this.checkInDate, mCheckOut.format('YYYY-MM-DD'));
-            }
-            else {
-                return new Model(this.checkInDate, this.checkOutDate, v[1]);
-            }
-        }
-
-        return new Model(this.checkInDate, this.checkOutDate, 'Invalid nights count replaced');
+        return new Model(this.checkInDate, this.checkOutDate);
     };
 
     Model.prototype.nightsCount = function () {
-        return moment(this.checkOutDate, 'YYYY-MM-DD').diff(
-            moment(this.checkInDate, 'YYYY-MM-DD'),
+        return moment(this.checkOutDate, fmt()).diff(
+            moment(this.checkInDate, fmt()),
             'days'
         );
     };
--- tests/Model.spec.js
+++ tests/Model.spec.js
@@ -19,61 +19,59 @@ describe.only('Model', function () {
     });
 
     describe('.prototype', function () {
-        var m = new Model('2014-09-24', '2014-09-30'),
-            e = new Environment(false, '2014-09-24');
-
         describe('.newCheckIn', function () {
-            it('on empty value refuses the change', function () {
-                assertValue(m.newCheckIn('', e), '2014-09-24', '2014-09-30');
+            describe('when zero nights aren\'t allowed', function () {
+                var m = new Model('2014-09-24', '2014-09-30'),
+                    e = new Environment(false, '2014-09-24');
+
+                it('on empty value refuses the change', function () {
+                    assertValue(m.newCheckIn('', e), '2014-09-24', '2014-09-30');
+                });
+
+                it('works in identity case', function () {
+                    assertValue(m.newCheckIn('2014-09-24', e), '2014-09-24', '2014-09-30');
+                });
+
+                it('on value after check-out moves the check-out', function () {
+                    assertValue(m.newCheckIn('2014-10-01', e), '2014-10-01', '2014-10-02');
+                });
+
+                it('on check-out value moves the check-out', function () {
+                    assertValue(m.newCheckIn('2014-09-30', e), '2014-09-30', '2014-10-01');
+                });
             });
 
-            it('works in identity case', function () {
-                assertValue(m.newCheckIn('2014-09-24', e), '2014-09-24', '2014-09-30');
+            describe('when zero nights are allowed', function () {
+                var m = new Model('2014-09-24', '2014-09-30'),
+                    e = new Environment(true, '2014-09-24');
+
+                it('allows check-out value', function () {
+                    assertValue(m.newCheckIn('2014-09-30', e), '2014-09-30', '2014-09-30');
+                });
+
+                it('reverts the change if the value is before today', function () {
+                    assertValue(m.newCheckIn('2014-09-23', e), '2014-09-24', '2014-09-30');
+                });
             });
 
-            it('on value after check-out moves the check-out, preserving nights', function () {
-                assertValue(m.newCheckIn('2014-10-01', e), '2014-10-01', '2014-10-07');
-            });
+            describe('for longger stays', function () {
+                var m = new Model('2014-09-24', '2014-09-30'),
+                    e = new Environment(false, '2014-08-01');
 
-            it('on check-out value moves the check-out', function () {
-                assertValue(m.newCheckIn('2014-09-30', e), '2014-09-30', '2014-10-01');
-            });
+                it('allows 27 nights stay', function () {
+                    assertValue(m.newCheckIn('2014-09-03', e), '2014-09-03', '2014-09-30');
+                });
 
-            it('allows check-out value when zero nights are allowed', function () {
-                assertValue(
-                    m.newCheckIn('2014-09-30', new Environment(true, '2014-09-24')),
-                    '2014-09-30',
-                    '2014-09-30'
-                );
-            });
-
-            it('reverts the change if the value is before today', function () {
-                assertValue(
-                    m.newCheckIn('2014-09-23', e),
-                    '2014-09-24',
-                    '2014-09-30',
-                    'Can\'t check in earlier than today'
-                );
-            });
-
-            it('allows 27 nights stay', function () {
-                assertValue(
-                    m.newCheckIn('2014-09-03', new Environment(false, '2014-08-01')),
-                    '2014-09-03',
-                    '2014-09-30'
-                );
-            });
-
-            it('moves the check-out if the stay is 28 nights', function () {
-                assertValue(
-                    m.newCheckIn('2014-09-02', new Environment(false, '2014-08-01')),
-                    '2014-09-02',
-                    '2014-09-03'
-                );
+                it('moves the check-out if the stay is 28 nights', function () {
+                    assertValue(m.newCheckIn('2014-09-02', e), '2014-09-02', '2014-09-03');
+                });
             });
         });
 
         describe('.newCheckOut', function () {
+            var m = new Model('2014-09-24', '2014-09-30'),
+                e = new Environment(false, '2014-09-24');
+
             it('yeilds a new check-out day in a valid case', function () {
                 assertValue(m.newCheckOut('2014-09-25', e), '2014-09-24', '2014-09-25');
             });
@@ -92,36 +90,9 @@ describe.only('Model', function () {
             });
         });
 
-        describe('.newNights', function () {
-            it('moves the check-out date', function () {
-                assertValue(m.newNights(1, e), '2014-09-24', '2014-09-25');
-            });
-
-            it('complains about an invalid integer', function () {
-                assertValue(
-                    m.newNights('bla', e),
-                    '2014-09-24',
-                    '2014-09-30',
-                    'Invalid nights count replaced'
-                );
-            });
-
-            it('complains about a huge integer', function () {
-                assertValue(
-                    m.newNights(10000, e),
-                    '2014-09-24',
-                    '2014-09-30',
-                    'Invalid nights count replaced'
-                );
-            });
-
-            it('does the common validition and correction', function () {
-                assertValue(m.newNights(99, e), '2014-09-24', '2014-09-30');
-            });
-        });
-
         describe('.nightsCount', function () {
             it('returns the difference between the check-in and check-out', function () {
+                var m = new Model('2014-09-24', '2014-09-30');
                 assert.strictEqual(m.nightsCount(), 6);
             });
         });
