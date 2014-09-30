8ead0bac8dce634adccbd326f08deeb69e3a696c
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-09-30 09:01:56 +0200

    Add the date-picker-wiring placeholder

---
+++ www/date-picker-wiring.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    // var checkInWidget = DOM.find('.period-of-stay-check-in input[type=date]'),
+    //     checkOutWidget = DOM.find('.period-of-stay-check-out input[type=date]');
+}());
