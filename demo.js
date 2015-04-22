167f40c24c8f74155a6d77f244aef83b23623b49
Pavel Kilin <pavel@asdf404.net>
2015-04-22 17:10:36 +0600

    Removed translations from demo

--- demo.js
+++ demo.js
@@ -13,13 +13,7 @@
 
                     onChange: function (model) {
                         this.setState({model: model});
-                    }.bind(this),
-                    locales: 'en',
-                    messages: {
-                        period: '{count, plural, =0 {Single day} =1 {1 night} other {# nights}}',
-                        checkInDay: 'Check-in day',
-                        checkOutDay: 'Check-out day'
-                    }
+                    }.bind(this)
                 });
             },
 
