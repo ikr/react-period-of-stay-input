c08a68f5655adf1809037d90c6e0aca8b7250cd3
Ivan Krechetov <ikr@ikr.su>
2018-04-09 20:13:05 +0200

    Reduce boilerplate a bit

--- browser.js
+++ browser.js
@@ -1,7 +1,3 @@
-(function () {
-    'use strict'
-
-    exports.Klass = require('./src/PeriodOfStayInput')
-    exports.Model = require('./src/Model')
-    exports.Environment = require('./src/Environment')
-}())
+exports.Klass = require('./src/PeriodOfStayInput')
+exports.Model = require('./src/Model')
+exports.Environment = require('./src/Environment')
--- src/intlMessages.js
+++ src/intlMessages.js
@@ -1,23 +1,19 @@
-(function () {
-    'use strict'
+module.exports = function () {
+    return {
+        en: {
+            'react-period-of-stay-input': {
+                period: '{count, plural, =0 {Single day} =1 {1 night} other {# nights}}',
+                checkInDay: 'Check-in day',
+                checkOutDay: 'Check-out day'
+            }
+        },
 
-    module.exports = function () {
-        return {
-            en: {
-                'react-period-of-stay-input': {
-                    period: '{count, plural, =0 {Single day} =1 {1 night} other {# nights}}',
-                    checkInDay: 'Check-in day',
-                    checkOutDay: 'Check-out day'
-                }
-            },
-
-            de: {
-                'react-period-of-stay-input': {
-                    'period': '{count, plural, =0 {Ein Tag} =1 {1 Nacht} other {# Nächte}}',
-                    'checkInDay': 'Anreise',
-                    'checkOutDay': 'Abreise'
-                }
+        de: {
+            'react-period-of-stay-input': {
+                'period': '{count, plural, =0 {Ein Tag} =1 {1 Nacht} other {# Nächte}}',
+                'checkInDay': 'Anreise',
+                'checkOutDay': 'Abreise'
             }
         }
     }
-}())
+}
