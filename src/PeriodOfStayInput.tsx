44582d13a3f95a08b84a6adc96a9113f127be2ae
Ivan Krechetov <ikr@ikr.su>
2018-07-27 16:36:05 +0200

    Render the top-level element

--- src/PeriodOfStayInput.tsx
+++ src/PeriodOfStayInput.tsx
@@ -2,14 +2,21 @@ import * as React from 'react'
 import Model from './Model'
 import Environment from './Environment'
 
-export default class PeriodOfStayInput extends React.Component {
+export default class PeriodOfStayInput extends React.Component<Props> {
     render() {
-        return null
+        const className = 'period-of-stay-input' + (
+            this.props.className ? ` ${this.props.className}` : ''
+        )
+
+        return (
+            <div {...{ className }}>
+            </div>
+        )
     }
 }
 
 export interface Props {
     model: Model,
     environment: Environment,
-    className: string
+    className?: string
 }
--- tests/PeriodOfStayInput.spec.tsx
+++ tests/PeriodOfStayInput.spec.tsx
@@ -8,22 +8,34 @@ import Model from '../src/Model'
 import intlMessages from '../src/intlMessages'
 import PeriodOfStayInput, { Props } from '../src/PeriodOfStayInput'
 
-function props(): Props {
-    return {
-        model: new Model(new Day('2014-09-26'), new Day('2014-09-27')),
-        environment: new Environment(false, new Day('2014-09-26')),
-        className: 'ad-hoc'
+describe('PeriodOfStayInput for 1+ nights', () => {
+    function props(): Props {
+        return {
+            model: new Model(new Day('2014-09-26'), new Day('2014-09-27')),
+            environment: new Environment(false, new Day('2014-09-26')),
+            className: 'ad-hoc'
+        }
     }
-}
 
-describe('PeriodOfStayInput', () => {
     const wrapper = mount(
         <IntlProvider locale='ru' messages={intlMessages().en}>
             <PeriodOfStayInput {...props()} />
         </IntlProvider>
-    ).find('Dropdown')
+    ).find('PeriodOfStayInput')
 
-    it.skip('fails for now', () => {
-        assert.fail('!')
+    describe('top-level element', () => {
+        const divWrapper = wrapper.childAt(0)
+
+        it('is a div', () => {
+            assert.strictEqual(divWrapper.type(), 'div')
+        })
+
+        it('has the static class assigned', () => {
+            assert(divWrapper.hasClass('period-of-stay-input'))
+        })
+
+        it('has the configurable class assigned', () => {
+            assert(divWrapper.hasClass('ad-hoc'))
+        })
     })
 })
