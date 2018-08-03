ec023dc37e0e91af6bf11086839430e571afa555
Ivan Krechetov <ikr@ikr.su>
2018-08-03 14:41:30 +0200

    Experiment with the DatePicker localization

--- src/demo.tsx
+++ src/demo.tsx
@@ -2,21 +2,30 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import DatePicker from 'react-datepicker'
 import insertCss from 'insert-css'
+import * as moment from 'moment'
+import 'moment/locale/de-ch'
 import { css } from './index'
 
-class Container extends React.Component {
+class Container extends React.Component<{}, State> {
     constructor(props: any) {
         super(props)
-        this.state = {}
+        this.state = { unixSeconds: moment.utc().unix() }
     }
 
     render() {
         return (
             <div>
-                <DatePicker onChange={() => null} />
+                <DatePicker
+                    locale='de-ch'
+                    selected={moment.unix(this.state.unixSeconds)}
+                    onChange={() => null} />
             </div>
         )
     }
+}
+
+interface State {
+    unixSeconds: number
 }
 
 insertCss(css)
