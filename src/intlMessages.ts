1d622e57d1f2d0a60550287ad052fb2ac5eb036a
Ivan Krechetov <ikr@ikr.su>
2018-08-07 16:48:01 +0200

    Tinker

--- src/intlMessages.ts
+++ src/intlMessages.ts
@@ -2,18 +2,18 @@ import Locale from './Locale'
 
 export default function (): { [locale in Locale]: { [key: string]: string } } {
     return {
-        en: {
+        [Locale.EN]: {
             'react-period-of-stay-input.period': '{count, plural, =0 {Single day} =1 {1 night} other {# nights}}',
             'react-period-of-stay-input.checkInDay': 'Check-in day',
             'react-period-of-stay-input.checkOutDay': 'Check-out day'
         },
 
-        de: {
+        [Locale.DE]: {
             'react-period-of-stay-input.period': '{count, plural, =0 {Ein Tag} =1 {1 Nacht} other {# Nächte}}',
             'react-period-of-stay-input.checkInDay': 'Anreise',
             'react-period-of-stay-input.checkOutDay': 'Abreise'
         },
-        zh: {
+        [Locale.ZH]: {
             'react-period-of-stay-input.period': '{count, plural, =0 {一晚} =1 {1 晚} other {# 晚}}',
             'react-period-of-stay-input.checkInDay': '入住日期',
             'react-period-of-stay-input.checkOutDay': '退房日期'
