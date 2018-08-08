9a1ceb295f8f4978d963b3988f59b1747cd89f62
Ivan Krechetov <ikr@ikr.su>
2018-08-08 16:32:29 +0200

    Put two more classes into the public API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as css } from './css'
+export { default as Locale } from './Locale'
+export { default as Day } from './Day'
 export { default as Model } from './Model'
 export { default as Environment } from './Environment'
 export { default as PeriodOfStayInput } from './PeriodOfStayInput'
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,11 @@
 import * as assert from 'assert'
 import * as api from '../src/index'
 import css from '../src/css'
+import Day from '../src/Day'
 import Model from '../src/Model'
 import Environment from '../src/Environment'
 import PeriodOfStayInput from '../src/PeriodOfStayInput'
+import Locale from '../src/Locale'
 
 describe('top-level API', () => {
     it('exposes the css submodule', () => {
@@ -20,5 +22,13 @@ describe('top-level API', () => {
 
     it('exposes the Environment', () => {
         assert.strictEqual(api.Environment, Environment)
+    })
+
+    it('exposes the Day', () => {
+        assert.strictEqual(api.Day, Day)
+    })
+
+    it('exposes the Locale', () => {
+        assert.strictEqual(api.Locale, Locale)
     })
 })
