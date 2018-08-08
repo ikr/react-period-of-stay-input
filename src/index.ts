a9896458d271dde5211c1e1ed919df10cde6bd26
Ivan Krechetov <ikr@ikr.su>
2018-08-08 12:19:46 +0200

    Expand the public API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as css } from './css'
+export { default as Model } from './Model'
+export { default as Environment } from './Environment'
+export { default as PeriodOfStayInput } from './PeriodOfStayInput'
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,24 @@
 import * as assert from 'assert'
 import * as api from '../src/index'
 import css from '../src/css'
+import Model from '../src/Model'
+import Environment from '../src/Environment'
+import PeriodOfStayInput from '../src/PeriodOfStayInput'
 
 describe('top-level API', () => {
     it('exposes the css submodule', () => {
         assert.strictEqual(api.css, css)
+    })
+
+    it('exposes the main React component', () => {
+        assert.strictEqual(api.PeriodOfStayInput, PeriodOfStayInput)
+    })
+
+    it('exposes the Model', () => {
+        assert.strictEqual(api.Model, Model)
+    })
+
+    it('exposes the Environment', () => {
+        assert.strictEqual(api.Environment, Environment)
     })
 })
