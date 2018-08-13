e643a707c3ca31d6b87db5dbed9910d3899ac553
Ivan Krechetov <ikr@ikr.su>
2018-08-13 12:32:55 +0200

    Annotate the Environment construction

--- src/demo.tsx
+++ src/demo.tsx
@@ -18,10 +18,17 @@ import {
 } from './index'
 
 // In order to display localized messages, react-intl needs the locale definitions data, describing
-// the formats, and the pluralization/gender rules.
+// the number and date formats, and the pluralization/gender rules.
 //
 addLocaleData(de)
 
+// The Environment describes the context where the period of stay input is used. The Environment
+// determines two things: 1) is the same-day check-out allowed? 2) what's the minimal check-in date?
+// Normally, the minimal check-in is the “today” or “tomorrow” date. However, the author had an
+// exotic requirement of having the minimal check-in being “yesterday”, in order to allow the
+// booking of a 1-night stay for late night (after 00:00:00) passengers whose flight was delayed or
+// cancelled.
+//
 const environment = new Environment(ZeroNightsPolicy.ALLOW, new Day('1979-11-16'))
 
 // That function manipulates the DOM, in order to detect the feature, and is therefore slow. You'd
